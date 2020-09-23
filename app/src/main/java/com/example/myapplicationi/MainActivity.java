68f9304a7d10f59f2e77bee5cb719e318549e5c6
Rahul Anand <49114833+rahula-1995@users.noreply.github.com>
2020-09-23 10:42:46 +0530

    shared preferences

--- app/src/main/java/com/example/myapplicationi/MainActivity.java
+++ app/src/main/java/com/example/myapplicationi/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.graphics.Color;
 import android.net.Uri;
 import android.nfc.Tag;
@@ -17,6 +18,8 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = "logging";
+    private static final String SHAREDPREFS = "sharedprefs";
+    private static final String KEYNAME ="keyname" ;
     EditText nameedit;
     Button button;
     boolean check;
@@ -91,12 +94,39 @@ public class MainActivity extends AppCompatActivity {
     protected void onResume() {
         super.onResume();
         Log.i(TAG,"mainactivity resume");
+        restoreData();
+    }
+    private void restoreData() {
+        //open the file
+        SharedPreferences preferences = getSharedPreferences(SHAREDPREFS,MODE_PRIVATE);
+
+        //read the file
+        String name = preferences.getString(KEYNAME,"");
+        //String pwd = preferences.getString(KEYPWD,"");
+        //restore the datq in edittexts
+        nameedit.setText(name);
+        //pwdEditText.setText(pwd);
     }
 
     @Override
     protected void onPause() {
         super.onPause();
         Log.i(TAG,"mainactivity pause");
+        saveData();
+    }
+    private void saveData() {///test case --onpause --if data is saved successfully then i should be able to open the file and see the data stored in that file
+        //get the data from edittexts
+        String name = nameedit.getText().toString();
+        //String pwd = pwdEditText.getText().toString();
+        //create a file sharedprefs
+        SharedPreferences preferences = getSharedPreferences(SHAREDPREFS,MODE_PRIVATE);
+        //open that file
+        SharedPreferences.Editor editor = preferences.edit();
+        //write to file
+        editor.putString(KEYNAME,name);
+        //editor.putString(KEYPWD,pwd);
+        //save the file
+        editor.commit();
     }
 
     @Override
